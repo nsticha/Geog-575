7656757fb57c38de4a5dbd9a7a2bbf4220b2af99
nsticha <nsticha@wisc.edu>
2023-04-11 12:23:40 -0500

    unit 3

--- unit3/boilerplate/js/main.js
+++ unit3/boilerplate/js/main.js
@@ -1,161 +1,46 @@
 //execute script when window is loaded
-window.onload = function(){
+window.onload = setMap
 
-	//SVG dimension variables
-    var w = 900, h = 500;
+// set up choropleth map
+function setMap(){
+    // creates map frame
+    var width = 600,
+    height = 600;
 
-    //Example 1.5 line 1...container block
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //put a new svg in the body
-        .attr("width", w) //assign the width
-        .attr("height", h) //assign the height
-        .attr("class", "container") //assign a class name
-        .style("background-color", "rgba(0,0,0,0.2)"); //svg background color
+//create new svg container for our map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class","map")
+        .attr("width", width)
+        .attr("height", height);
+//create albers projection
+    var projection = d3.geoAlbers()
+        .center ([-66, -17.5])
+        .rotate([-2,0,0])
+        .parallels([-35,25])
+        .scale(440)
+        .translate([width / 2, height / 2]);
+//converts the projection into a usable object
+        var path = d3.geoPath()
+            .projection(projection);
+    var promises = [
+        d3.csv("data/CSA.csv"),
+        d3.json("data/southAmerica.topojson")
+    ]
 
-    //Example 1.8 line 1...innerRect block
-    var innerRect = container.append("rect")
-        .datum(400) //a single value is a DATUM
-        .attr("width", function(d){ //rectangle width
-            return d * 2; //400 * 2 = 800
-        })
-        .attr("height", function(d){ //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
+    Promise.all(promises).then(callback);
 
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
-
-    //above Example 2.8 line 20
-    var x = d3.scaleLinear() //create the scale
-        .range([90, 810]) //output min and max
-        .domain([0, 4]); //input min and max
-
-    //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
-
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
-   //scale for circles center y coordinate
-   var y = d3.scaleLinear()
-   .range([450, 50])
-   .domain([
-       0, 700000
-   ]);
+    function callback(data){
 
 
-    //color scale generator 
-    var color = d3.scaleLinear()
-        .range([
-            "#33FFFF",
-            "#6600CC"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
-
-    //Example 2.6 line 3
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the index to place each circle horizontally
-            return x(i);
-        })
-        .attr("cy", function(d){
-            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
-
-        //add axis
-        var yAxis = d3.axisLeft(y);
-
-        //create axis g element and add axis
-        var axis = container.append("g")
-                .attr("class", "axis")
-                .attr("transform", "translate(50, 0)")
-                .call(yAxis);
-                
-        var axis = container.append("g")
-                .attr("class", "axis")
-                .attr("transform", "translate(50, 0)")
-                .call(yAxis);
-
-         var title = container.append("text")
-                .attr("class", "title")
-                .attr("text-anchor", "middle")
-                .attr("x", 450)
-                .attr("y", 30)
-                .text("Wisconsin City Populations");
-
-        var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population) -5;
-        })
-        .text(function(d){
-            return d.city;
-        });
-
-        var format = d3.format(","); 
-
-        var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "18") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); // use format variable to format the numbers withh commas
-        });
-
-
-};
+        var csvData = data[0],
+            centandsouth = data[1];
+        
+        var southAmerica = topojson.feature(centandsouth, centandsouth.objects.southAmerica);
+// add south american countries to the map
+        var countries = map.append("path")
+            .datum(southAmerica)
+            .attr("class","countries")
+            .attr("d",path);
+    }
+}
